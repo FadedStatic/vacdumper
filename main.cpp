0f5aabf4c8458824d8bb2526dd680793e1d2dbe5
Verbum <static.faded.cpp@gmail.com>
2024-01-25 23:21:25 -0500

    Final ver.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,26 @@
-#include <Windows.h>
-#include <iostream>
-#include <thread>
-#include <string>
-#include "patternscanner/scanner.hpp"
 #include <fstream>
-#include <algorithm>
+#include <string>
 
+#include "patternscanner/scanner.hpp"
 #include "minhook/MinHook.h"
 
-using vaclive_stub = char(__stdcall*)(DWORD* src, char flag);
-vaclive_stub vaclive_old;
+using vaclm_stub = char(__stdcall*)(DWORD* src, char flag);
+vaclm_stub vaclm_old;
+int vac_counter = 15;
 
-char __stdcall vaclive_sub_hook(DWORD* src, char flag) {
-    if (reinterpret_cast<void*>(src[6]) != nullptr && src[5] > 8192) {
-        OutputDebugStringA(std::vformat("VACDUMPER: Trying to fetch new module... {:d}\n", std::make_format_args((int)src[5])).c_str());
-        const auto* module_start = reinterpret_cast<std::uint8_t*>(src[6]);
-        OutputDebugStringA("VACDUMPER: Where thi hoe brakin.");
-        const auto mod_sz = static_cast<std::uintptr_t>(src[5]);
-        OutputDebugStringA("VACDUMPER: Where thi hoe brakin 1.");
-        const auto module_dos_header = *reinterpret_cast<const IMAGE_DOS_HEADER*>(module_start);
-        OutputDebugStringA("VACDUMPER: Where thi hoe brakin 2.");
-        const auto module_nt_headers = *reinterpret_cast<const IMAGE_NT_HEADERS*>(module_start + module_dos_header.e_lfanew);
-        OutputDebugStringA(std::vformat("VACDUMPER: Module {:d} was loaded, dumping...\n", std::make_format_args(module_nt_headers.OptionalHeader.SizeOfCode)).c_str());
+char __stdcall vaclm_stub_hook(DWORD* src, char flag) {
+    if (reinterpret_cast<void*>(src[6]) != nullptr) {
+        OutputDebugStringA(std::vformat("VACDUMPER: Module {:d} was loaded, dumping...\n", std::make_format_args(vac_counter)).c_str());
         CreateDirectoryA("C:\\vac_modules", nullptr);
 
-        std::ofstream file("C:\\vac_modules\\vac_module_" + std::to_string(module_nt_headers.OptionalHeader.SizeOfCode) + ".dll", std::ios::out | std::ios::binary);
-        file.write(reinterpret_cast<char*>(src[6]), mod_sz);
+        std::ofstream file("C:\\vac_modules\\vac_module_" + std::to_string(vac_counter) + ".dll", std::ios::out | std::ios::binary);
+        file.write(reinterpret_cast<char*>(src[6]), src[5]);
         file.close();
-        OutputDebugStringA(std::vformat("VACDUMPER: Saved module {:d} to {:s}\r\n", std::make_format_args(module_nt_headers.OptionalHeader.SizeOfCode, "C:\\vac_modules\\vac_module_" + std::to_string(module_nt_headers.OptionalHeader.SizeOfCode) + ".dll")).c_str());
+        OutputDebugStringA(std::vformat("VACDUMPER: Saved module {:d} to {:s}\r\n", std::make_format_args(vac_counter, "C:\\vac_modules\\vac_module_" + std::to_string(vac_counter) + ".dll")).c_str());
+        vac_counter++;
     }
 
-    return vaclive_old(src, flag);
+    return vaclm_old(src, flag);
 }
 
 int main() {
@@ -42,18 +32,19 @@ int main() {
         return 0;
     }
 
-    const auto vaclive_stub = util::get_prologue(curr_proc, phmodstr_xrefs[0].loc);
-    if (!vaclive_stub.has_value()) {
-        OutputDebugStringA("VACDUMPER: Error, Could not find vac live stub\n");
+    const auto vaclm_stub = util::get_prologue(curr_proc, phmodstr_xrefs[0].loc);
+    if (!vaclm_stub) {
+        OutputDebugStringA("VACDUMPER: Error, Could not find vac load module stub\n");
         return 0;
     }
 
-    OutputDebugStringA(std::vformat("VACDUMPER: VAC stub found at: 0x{:02X} (0x{:02X})\r\n", std::make_format_args(*vaclive_stub, *vaclive_stub - reinterpret_cast<uintptr_t>(GetModuleHandleA("steamservice.dll")))).c_str());
+    OutputDebugStringA(std::vformat("VACDUMPER: VAC stub found at: 0x{:02X} (0x{:02X})\r\n", std::make_format_args(*vaclm_stub, *vaclm_stub - reinterpret_cast<uintptr_t>(GetModuleHandleA("steamservice.dll")))).c_str());
 
+
+    jmp_found:
     MH_Initialize();
-    MH_CreateHook(reinterpret_cast<LPVOID>(*vaclive_stub), &vaclive_sub_hook, reinterpret_cast<PVOID*>(&vaclive_old));
-    MH_EnableHook(reinterpret_cast<LPVOID>(*vaclive_stub));
-
+    MH_CreateHook(reinterpret_cast<LPVOID>(*vaclm_stub), &vaclm_stub_hook, reinterpret_cast<PVOID*>(&vaclm_old));
+    MH_EnableHook(reinterpret_cast<LPVOID>(*vaclm_stub));
     OutputDebugStringA("VACDUMPER: Hook initialized.\n");
 
     return 0;
